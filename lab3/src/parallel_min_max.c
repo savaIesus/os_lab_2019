fb6c48c142b365903167f5769eada9aec10b44ab
savaIesus <saveliy24032005@gmail.com>
2024-10-26 06:08:59 +0000

    без 5 номера

--- lab3/src/parallel_min_max.c
+++ lab3/src/parallel_min_max.c
@@ -20,6 +20,8 @@ int main(int argc, char **argv) {
     bool with_files = false;
 
     while (true) {
+        int current_optind = optind ? optind : 1;
+
         static struct option options[] = {
             {"seed", required_argument, 0, 0},
             {"array_size", required_argument, 0, 0},
@@ -36,62 +38,95 @@ int main(int argc, char **argv) {
         switch (c) {
             case 0:
                 switch (option_index) {
-                    case 0: seed = atoi(optarg); break;
-                    case 1: array_size = atoi(optarg); break;
-                    case 2: pnum = atoi(optarg); break;
-                    case 3: with_files = true; break;
-                    default: printf("Индекс %d выходит за пределы опций\n", option_index);
+                    case 0:
+                        seed = atoi(optarg);
+                        break;
+                    case 1:
+                        array_size = atoi(optarg);
+                        break;
+                    case 2:
+                        pnum = atoi(optarg);
+                        break;
+                    case 3:
+                        with_files = true;
+                        break;
+                    default:
+                        printf("Index %d is out of options\n", option_index);
                 }
                 break;
             case 'f':
-                with_files = true; break;
+                with_files = true;
+                break;
+
             case '?':
                 break;
+
             default:
-                printf("getopt вернул код символа 0%o?\n", c);
+                printf("getopt returned character code 0%o?\n", c);
         }
     }
 
     if (optind < argc) {
-        printf("Есть хотя бы один аргумент без опции\n");
+        printf("Has at least one no option argument\n");
         return 1;
     }
 
     if (seed == -1 || array_size == -1 || pnum == -1) {
-        printf("Использование: %s --seed \"num\" --array_size \"num\" --pnum \"num\"\n", argv[0]);
+        printf("Usage: %s --seed \"num\" --array_size \"num\" --pnum \"num\" \n",
+               argv[0]);
         return 1;
     }
 
     int *array = malloc(sizeof(int) * array_size);
-    if (array == NULL) {
-        perror("Не удалось выделить память");
-        return 1;
-    }
-
     GenerateArray(array, array_size, seed);
-    
     int active_child_processes = 0;
+
     struct timeval start_time;
     gettimeofday(&start_time, NULL);
+
+    // Объявление массива для pipe
+    int pipe_fd[2];
+    
+    // Создание pipe
+    if (pipe(pipe_fd) == -1) {
+        perror("pipe");
+        exit(EXIT_FAILURE);
+    }
 
     for (int i = 0; i < pnum; i++) {
         pid_t child_pid = fork();
         if (child_pid >= 0) {
             active_child_processes += 1;
             if (child_pid == 0) {
-                // Логика дочернего процесса здесь
-                // Выполнить вычисления и отправить результаты через IPC
-                exit(0); // Убедитесь, что дочерний процесс завершает работу корректно
+                // Child process
+                close(pipe_fd[0]); // Закрываем конец чтения в дочернем процессе
+
+                int start_index = (array_size / pnum) * i;
+                int end_index = (i == pnum - 1) ? array_size : start_index + (array_size / pnum);
+                
+                int local_min = INT_MAX;
+                int local_max = INT_MIN;
+
+                for (int j = start_index; j < end_index; j++) {
+                    if (array[j] < local_min) local_min = array[j];
+                    if (array[j] > local_max) local_max = array[j];
+                }
+
+                // Записываем результаты в pipe
+                write(pipe_fd[1], &local_min, sizeof(int));
+                write(pipe_fd[1], &local_max, sizeof(int));
+                
+                close(pipe_fd[1]); // Закрываем конец записи после записи
+                return 0; // Завершаем дочерний процесс
             }
         } else {
-            perror("Ошибка при создании процесса!");
-            free(array);
+            printf("Fork failed!\n");
             return 1;
         }
     }
 
     while (active_child_processes > 0) {
-        wait(NULL); // Ждать любой дочерний процесс
+        wait(NULL); // Ждем завершения всех дочерних процессов
         active_child_processes -= 1;
     }
 
@@ -99,19 +134,30 @@ int main(int argc, char **argv) {
     min_max.min = INT_MAX;
     min_max.max = INT_MIN;
 
-    // Чтение результатов из файлов или каналов здесь
+    for (int i = 0; i < pnum; i++) {
+        int min_from_pipe, max_from_pipe;
+
+        // Читаем результаты из pipe
+        read(pipe_fd[0], &min_from_pipe, sizeof(int));
+        read(pipe_fd[0], &max_from_pipe, sizeof(int));
+
+        if (min_from_pipe < min_max.min) min_max.min = min_from_pipe;
+        if (max_from_pipe > min_max.max) min_max.max = max_from_pipe;
+    }
+
+    close(pipe_fd[0]); // Закрываем конец чтения после использования
 
     struct timeval finish_time;
     gettimeofday(&finish_time, NULL);
 
-    double elapsed_time = (finish_time.tv_sec - start_time.tv_sec) * 1000.0 +
-                          (finish_time.tv_usec - start_time.tv_usec) / 1000.0;
+    double elapsed_time = (finish_time.tv_sec - start_time.tv_sec) * 1000.0;
+    elapsed_time += (finish_time.tv_usec - start_time.tv_usec) / 1000.0;
 
     free(array);
 
     printf("Min: %d\n", min_max.min);
     printf("Max: %d\n", min_max.max);
-    printf("Время выполнения: %f мс\n", elapsed_time);
+    printf("Elapsed time: %fms\n", elapsed_time);
     
     return 0;
 }
